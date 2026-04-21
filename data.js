a3ade984e0630c8b7e84fd7f642c9b59071b2dca
alphaman2026 <gwmaw1@naver.com>
2026-04-21 09:21:50 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,14 @@
 const orgData = [
-    { "name": "김삼성", "role": "Foundry Head" },
-    { "name": "이반도", "role": "Marketing Manager" }
+    {
+        "name": "도정호",
+        "role": "기술총괄"
+    },
+    {
+        "name": "임연호",
+        "role": "Software 총괄"
+    },
+    {
+        "name": "김주철",
+        "role": "사내기자단"
+    }
 ];
